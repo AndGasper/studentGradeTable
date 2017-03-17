9967f8a949f8a92ec91335e8f146ad0a0c959f85
Andres Gasper <Andres Gasper>
2017-03-17 09:28:16 -0700

    Styling is now complete for wire frame. The functions within script.js are nearly complete. IN PROGRESS: Compliance with JSDoc style conventions

--- script.js
+++ script.js
@@ -1,42 +1,80 @@
 /**
  * Define all global variables here
- */
+//  */
+// var studentName = null;
+// var courseName = null;
+// var studentGrade = null;
+
 /**
  * student_array - global array to hold student objects
  * @type {Array}
  */
+var student_array = [];
 
 /**
  * inputIds - id's of the elements that are used to add students
  * @type {string[]}
  */
+var studentName = $("#studentName").val();
+var courseName = $("#course").val();
+var studentGrade = $("#studentGrade").val();
 
 /**
- * addClicked - Event Handler when user clicks the add button
+ * addClicked = Event Handler when user clicks the add button, should a
  */
-
+function addClicked() {
+    addStudent();
+};
 /**
  * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
  */
+function cancelClicked() {
+    clearAddStudentForm();
+};
 
 /**
  * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
  *
  * @return undefined
  */
-
+function addStudent() {
+    var student = {
+        "name": $("#studentName").val(),
+        "course": $("#course").val(),
+        "grade": $("#grade").val()
+    };
+    student_array.push(student);
+    return;
+}
 /**
  * clearAddStudentForm - clears out the form values based on inputIds variable
  */
-
+function clearAddStudentForm() {
+    $(studentName).val("");
+    $(courseName).val("");
+    $(studentGrade).val("");
+};
 /**
  * calculateAverage - loop through the global student array and calculate average grade and return that value
  * @returns {number}
  */
+function calculateAverage(student_array) {
+    var total = 0;
+    for (var i = 0; i <= student_array.length -1; i++) {
+        total += student_array[i];
+    }
+    var average = total/(student_array.length);
+    return average;
+}
 
 /**
  * updateData - centralized function to update the average and call student list update
  */
+function updateData() {
+    updateStudentList();
+
+
+}
 
 /**
  * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
@@ -47,12 +85,23 @@
  * into the .student_list tbody
  * @param studentObj
  */
+function addStudentToDom(studentObj) {
+    for (var i in studentObj) {
+        studentObj[i]
+    }
+}
 
 /**
  * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
  */
+function reset() {
+
+};
 
 
 /**
  * Listen for the document to load and reset the data to the initial state
- */
+ *  */
+document.addEventListener(load, function(event) {
+
+} )
