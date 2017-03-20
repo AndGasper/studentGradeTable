391a692fce9287a939f5e0975ea8b2a621660733
Andres Gasper <Andres Gasper>
2017-03-20 15:31:19 -0700

    Quick save. Got the deletion working. Need to double check that I am not still adding people twice

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ function removeStudentFromDom() {
     // console.log("Who is my parent row?");
     //console.log($(this).parent()[0]); // $(this).parent()[0] removes that entire row. The [0] index is the table row
     // console.log("Am I the index?", $(this).parent().index()); // Index of the row in the table for removing from student array
-    student_array.splice($(this).parent().index(),1); // Removes the student object entry from the student array
+    student_array[0].splice($(this).parent().index(),1); // Removes the student object entry from the student array
     $(this).parent()[0].remove();
     // console.log(student_array);
 
